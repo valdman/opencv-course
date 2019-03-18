e280021f6cefa433713dafab05a499bbc196ad40
Boris Valdman <boris.valdman@live.ru>
2019-03-18 13:29:37 +0300

    histogram drawer added

---
+++ histogram/src/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace cv;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+static Mat createHistogram(Mat& image) {
+  vector<Mat> bgr_planes;
+  split( image, bgr_planes );
+
+  int histSize = 256;
+
+  float range[] = { 0, 256 } ;
+  const float* histRange = { range };
+
+  bool uniform = true; bool accumulate = false;
+
+  Mat b_hist, g_hist, r_hist;
+
+  calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+  int hist_w = 512; int hist_h = 400;
+  int bin_w = cvRound( (double) hist_w/histSize );
+
+  Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+  normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+  for( int i = 1; i < histSize; i++ ) {
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                       Scalar( 255, 0, 0), 2, 8, 0  );
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                       Scalar( 0, 255, 0), 2, 8, 0  );
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                       Scalar( 0, 0, 255), 2, 8, 0  );
+  }
+  return histImage;
+}
+
+int main( int argc, char** argv ) {
+  CommandLineParser parser( argc, argv, "{@input | ./assets/water_coins.jpg | input image}" );
+  Mat src = imread(parser.get<string>("@input"));
+
+  if( !src.data ) {
+     return -1; 
+  }
+
+  imshow("Histogram", createHistogram(src));
+  imshow("Original image", src);
+  waitKey(0);
+  return 0;
+}
